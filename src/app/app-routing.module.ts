046d6ca25f3a60923502c44db32990cfd1ae732e
Rafa <rr_urdaneta_12@hotmail.com>
2019-03-01 14:27:09 -0400

    ruteado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ChefinfoComponent } from './components/chefinfo/chefinfo.component';
+import { DetallesComponent } from './components/detalles/detalles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '',  component: HomeComponent },
+{ path: 'info', component: ChefinfoComponent},
+{ path: 'receta/:id', component: DetallesComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
